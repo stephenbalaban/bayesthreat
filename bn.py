ced98d0a99635534adee3cbbbc2c174fef4c0baa
stephen@stephenbalaban.com <mail@stephenbalaban.com>
2012-03-10 17:35:04 -0800

    moved bn_rogue to bn.py

--- bn.py
+++ bn.py
@@ -1,8 +1,14 @@
-from itertools import product, combinations
+import functools
+import itertools
 from math import log
-import copy
+
+it = itertools
+ft = functools
 
 DEBUG = False
+NEGCHAR = '!'
+SEPCHAR = ','
+PIPCHAR = '|'
 
 """
 Example BN with CPTs:
@@ -49,303 +55,332 @@ BN with continuous variables:
 
 """
 
-class RV(object):
-    def __init__(self, name,abbr):
-        self.name = name
-        self.abbr = abbr
+#
+# utilities
+#
 
-def neg(pr):
-    return 1 - pr
-
+mult = lambda a,b: a * b
+product = lambda l: functools.reduce(mult, l)
 identity = lambda x: x
-
-def sign_value(value, sign=True):
-    return (identity if sign else neg)(value)
-
-class BayesNode(object):
-    def __init__(self, rv, cpt, parents=[], sign=True):
-        self.sign = sign
-        self.name = rv.name
-        self.abbr = rv.abbr
-        self.rv = rv
-        self.parents = parents
-        self.set_sign(sign)
-        self.cpt = self.make_cpt(cpt, sign=sign)
-
-    def set_sign(self, sign=True):
-        self.name = self.name.replace('!', '') if sign else '!%s' % self.name if '!' not in self.name else self.name
-        self.abbr = self.abbr.replace('!', '')  if sign else '!%s' % self.abbr if '!' not in self.abbr else self.abbr
-        self.sign = sign
-        return self
-
-    def __repr__(self):
-        result = "{ %s }" % self.name#\n%s\n" % (self.name, self.print_cpt(self.cpt))
-        result = "{ %s }\n%s\n" % (self.name, self.print_cpt(self.cpt))
-        return result
-
-    def __nonzero__(self):
-        return bool(self.sign)
-
-    def get_cpt(self, key):
-        return sign_value(self.cpt[key], self.sign)
-
-    def neg(self):
-        new = copy.deepcopy(self)
-        return new.set_sign(not(new.sign))
-
-    def pr(self, rvs=(), given=(), full_dist={}):
-        """
-        No priors:
-            P(x_1, ..., x_n) = \Pi_{i=1]^n P(x_i | x_{i-1}, ..., x_n)
-        With Priors (loop over hidden variables):
-            Full distribution: 
-                P(x_1, x_2, x_3, x_4, x_5, ..., x_n)
-            Prior probabilities:
-                P( x_1, x_2 | x_3) = \alpha P(x_1, x_2, x_3) 
-                                   = \alpha \Sigma_{x_4} \Sigma_{x_5} ... \Sigma_{x_n} P(x_1, x_2, ..., x_n)
-        """
-        if given:
-            pr = self.calculate_prior_pr(rvs, given, full_dist=full_dist)
-        else:
-            pr = self.calculate_full_pr(rvs)
-        return pr
-
-    def rv_possibilities(cls, full_distribution, hidden_variables):
-        """ 
-        maps full_dist, hidden_vars -> all possible full distributions
-        iterating over the values of hidden variables (requires boolean rvs)
-        """
-        set_difference = list(set(full_distribution.values()) - set(hidden_variables))
-        print("set diff: %s" % set_difference)
-        # all possible characters to combine
-        possibilities = flatten([[hv, hv.neg()] for hv in hidden_variables])
-        # all combinations of those characters
-        combos = make_combinations(possibilities, len(hidden_variables), splitchar='!')
-        # those combos consed into a new list
-        final_combos = [combo + set_difference for combo in combos]
-        print("""
-
-        hidden vars:
-        %s
-
-        set diff:
-        %s
-        
-    -------------------
-        final combos:
-        %s
-    -------------------
-        combols: 
-        %s
-    -------------------
-        possibilitit
-        %s
-        
-    -------------------
-        
-        """ % (hidden_variables, set_difference, final_combos, combos, possibilities))
-        return final_combos
-
-    def calculate_prior_pr(self, rvs, given, full_dist):
-        """
-        """
-        hidden_vars = list(set(full_dist.values()) - set(rvs + given))
-        print('fd %s ' % full_dist)
-        print("hv %s " % (hidden_vars))
-        return sum([
-                self.calculate_full_pr(rvs) for rvs in self.rv_possibilities(full_dist, hidden_vars)
-        ])
-        pass
-
-    def calculate_full_pr(self, rvs):
-        """
-        """
-        if self in rvs:
-            this_node = filter(lambda n: n == self or n.neg() == self, rvs)[0]
-        else:
-            print(self)
-            print('rvs: %s' % rvs)
-            raise Exception("Couldn't use the rvs given... didn't find myself there!")
-        this_parents = list(filter(lambda n: n in self.parents, rvs))
-        if not this_parents:
-            this_parents_key = True
-        else:
-            this_parents_key = tuple([bool(p) for p in this_parents]) if len(this_parents) > 1 else bool(this_parents[0])
-        pr = self.get_cpt(this_parents_key)
-        if DEBUG:
-            print("""
-            this_node: %s 
-            this_node.sign: %s
-            this_parents:    %s
-            this_parents_key: %s
-            this_cpt: %s
-            """ % (this_node, this_node.sign, this_parents, this_parents_key, self.cpt))
-            print(pr)
-        return pr
-
-    def print_cpt(self, cpt):
-        """
-        A large pile of crufty hacks to print out the CPTs, string formatting
-        is fun!
-        """
-        rows = [self.print_row(item) for item in cpt.items()]
-        header_len = len(rows[0].split('\n')[0]) - 2
-        between = '+' + '-' * header_len + '+\n'
-        if self.parents:
-            header = '| ' + ''.join(['%s ' for p in self.parents]) + '| P(%s)  |\n' % self.abbr
-            header = header % tuple([p.abbr for p in self.parents])
-            result = '\n'.join([self.print_row(item) for item in cpt.items()])
-        else:
-            header = ('| P(%s)' + ' ' * (header_len - 5) + '|\n') % self.abbr
-            result = '\n'.join([self.print_row(item, no_parents=True) for item in cpt.items()])
-        return between + header + between + result
-
-    def print_row(self, cpt_item, no_parents=False):
-        """
-        maps tuple like: 
-            (True, 0.95) -> | 0.95 |
-            ((True, False, True), 0.90) -> | 1 0 1 | 0.90 | 
-        """
-        cpt_item = list(cpt_item)
-        # make body
-        if type(cpt_item[0]) in [tuple, list]:
-            cpt_item[0] = map(int, cpt_item[0])
-            row = '| ' + ''.join(['%s ' for var in cpt_item[0]]) + '|'
-            row = row % tuple(cpt_item[0])
-        else:
-            cpt_item[0] = int(cpt_item[0])
-            row = '| %s |' % cpt_item[0]
-        if no_parents:
-            row = '| %s     |' % cpt_item[1]
-        else:
-            row += ' %.3f |' % cpt_item[1]
-        # add rows in between
-        row += '\n+' + '-' * (len(row) - 2) + '+'
-        return row
-
-    def make_cpt(self, cpt, sign=True):
-        cpt_type = type(cpt)
-        if cpt_type not in [list, dict]:
-            raise Exception("Invalid CPT given, type(cpt): %s" % cpt_type)
-        f = self.make_dict_cpt if cpt_type == dict else self.make_list_cpt
-        r = f(cpt)
-        # negate values in dictionary if sign
-        return dict([(key, sign_value(val, sign)) for key, val in r.items()])
-
-    def make_dict_cpt(self, cpt):
-        result = {}
-        for k in cpt:
-            result[bool(k)] = cpt[k]
-        return result
-
-    def var_count(self, cpt_list):
-        return int(log(len(cpt_list), 2))
-
-    def make_list_cpt(self, cpt_list):
-        """ maps cpt_list [ pr, pr, pr ] -> dictionary  """
-        n = self.var_count(cpt_list)
-        keys = product([True,False], repeat=n) if n > 1 else [True, False]
-        result = dict(zip(keys, cpt_list))
-        return result
-
-
-def unique(splitchar="!"):
-    def result(tup):
-        return len(set(map(lambda x: x.name.split(splitchar)[-1], tup))) == len(map(lambda x: x.name.split(splitchar)[-1], tup))
-    return result
+pr_not = lambda pr: 1 - pr
 
 def flatten(l):
+    """
+    Takes a nested list of lists (two deep) and flattens it into a single list:
+    [[1,2],[3,4],[5,6]] -> [1,2,3,4,5,6]
+    """
     return [item for sublist in l for item in sublist]
 
-def make_combinations(possibilities, count, splitchar='!'):
-    return map(list, filter(unique(splitchar), list(combinations(possibilities, count))))
+##
+# Data Structure:
+#
+#   Node
+#       A node is a 4-tuple [name, cpt, abbr, parents]
+#   Bayesnet
+#       A Bayesnet is a dict of nodes keyed by their abbreviation
+#           'a': node_a
+#           'b': node_b
+##
 
-class BayesNet(object):
-    def __init__(self, *nodes):
-        self.nodes = dict(map(lambda n: (n.name, n), nodes))
-        self.abbrs = dict(map(lambda n: (n.abbr, n), nodes))
+#
+# Node functions
+#
 
-    def __repr__(self):
-        result = "BayesNet\n"
-        result += '\n'.join(map(str, self.nodes.values()))
-        return result
+def node(name, abbr, cpt, parents=[]):
+    """
+    A node is a 4-tuple:
+        name, constraint_pr_table, abbreviation, parents_list
+    e.g.
+        >>> bn.node('Female', 'f', { (): 0.5}, []) 
+        ['Female', {(): 0.5}, 'f', []]
+    """
+    return [name, cpt, abbr, parents]
 
-    def get(self, abbr):
-        return self.abbrs[abbr]
+def node_abbr_not(abbr, negchar=NEGCHAR):
+    """ 
+    Returns the opposite abbr of a given node
+    e.g.
+        >>> node_abbr_not('nodename')
+        '!nodename'
+        >>> node_abbr_not('!nodename')
+        'nodename'
+    """
+    if negchar in abbr:
+        return abbr.split(negchar)[0]
+    else:
+        return '!%s' % abbr
 
-    def name(self, name):
-        return self.nodes[name]
+def parents(node):
+    return node[3]
 
-    def pr(self, string):
-        """
-           returns  Pr( rv | given )
-        """
-        rvs, given = self.parse_query(string)
-        return capital_pi([node.pr(rvs, given, full_dist=self.nodes) for node in self.nodes.values()])
+def abbr(node):
+    return node[2]
 
-    def parse_query(self, query):
-        """
-        maps string query -> rv, given list
-        """
-        if '|' in query:
-            dirty_rvs, dirty_given = query.split('|')
-            given_strs = tuple([x.strip() for x in dirty_given.split(',')])
-        else:
-            dirty_rvs = query.split(',')
-            given_strs = tuple([])
-        rvs = self.parse_rvs(dirty_rvs)
-        given = self.parse_given(given_strs)
-        return (rvs, given)
+def name(node):
+    return node[0]
 
-    def abbr_pos_nodes(self, abbr_positive):
-        # generate nodes
-        print(abbr_positive)
-        rvs = [self.abbrs[abbr].set_sign(positive) for abbr, positive in abbr_positive]
-        return tuple(rvs)
+def cpt(node):
+    return node[1]
 
-    def parse_given(self, given_strs):
-        """ parses given strs into given rvs """
-        abbr_positive = [self.parse_string(rv) for rv in given_strs]
-        return tuple(self.abbr_pos_nodes(abbr_positive))
+def keys(node):
+    """
+    node -> cpt keys
+    """
+    return list(cpt(node).keys())
 
-    def parse_rvs(self, dirty_rvs):
-        """ parses dirty rvs to a list of rvs """
-        # remove spaces
-        rvs = [x.strip() for x in dirty_rvs]
-        # remove empty strings
-        rvs = filter(bool, rvs)
-        abbr_positive = [self.parse_string(rv) for rv in rvs]
-        return tuple(self.abbr_pos_nodes(abbr_positive))
+def node_pr(node, given_key=(), is_pos=True):
+    """ 
+    (node, given_list, is_positive) -> float
+    """
+    wrapper = identity if is_pos else pr_not
+    if given_key not in cpt(node):
+        raise KeyError("Key for cpt(node) not provided, got: %s" % str(given_key))
+    else:
+        return wrapper(cpt(node)[given_key])
 
-    def parse_string(self, rv_str):
-        """
-        maps node -> node_string, positive?
-             !a -> a, False
-             c  -> c, True
-        """
-        return (rv_str.replace('!', ''), '!' not in rv_str)
+def parse_sep(sepstr, sepchar=SEPCHAR):
+    """
+    >>> parse_sep('a,b,c')
+    ['a','b','c']
+    >>> parse_sep(',a,b,c') # no empty strings
+    ['a','b','c']
+    """
+    return list(filter(bool, [x.strip() for x in sepstr.split(sepchar)]))
 
-capital_pi = lambda l: reduce(lambda a,b: a*b, l)
+def negate(rvs, negchar=NEGCHAR):
+    """
+    rv_list -> neg_rv_list
 
-def make_earthquake_model():
-    erv = RV(name="Earthquake", abbr='e')
-    brv = RV("Burgler", 'b')
-    arv = RV("Alarm", 'a')
-    mrv = RV("MaryCalls", 'm')
+    >>> negate(['a', '!b', 'c'])
+    ['!a', 'b', '!c']
+    >>> negate(['a'])
+    ['a']
+    """
+    return list(map(lambda x: x.split(negchar)[-1] if negchar in x else 
+                                                        '!%s' % x, rvs))
 
-    jrv = RV("JohnCalls", 'j')
-    e = BayesNode(rv=erv, cpt=[0.002], parents=[]) # [0.002] expands to { 1: 0.002, 0: 1 - 0.002 }
-    b = BayesNode(brv, cpt={ True: 0.001 }) # no parentsj
-    a = BayesNode(arv, cpt=[0.95, 0.94, 0.29, 0.001], parents=[b, e])
-    m = BayesNode(mrv, cpt=[0.70, 0.01], parents=[a]) # [0.70, 0.01] expands to { 1: 0.70, 0: 0.01 }
-    j = BayesNode(jrv, cpt=[0.90, 0.05], parents=[a])
-    bn = BayesNet(e,b,a,j,m)
+def parse_query(st, pipechar=PIPCHAR, sepchar=SEPCHAR, do_negate=False):
+    """
+    query_str -> rvs, given
+    >>> parse_query('b | m,j')
+    ('b', ('m','j')) 
+    """
+    if pipechar in st:
+        dirty_rvs, dirty_given = st.split(pipechar)
+        given = parse_sep(dirty_given)
+    else:
+        dirty_rvs = st
+        given = []
+    rvs = parse_sep(dirty_rvs)
+    if do_negate:
+        rvs = negate(rvs)
+    return rvs, given
+
+def choose_key(query_list):
+    def choose(key):
+        return all([abbr in query_list for abbr in key])
+    return choose
+
+def first(l):
+    l = tuple(l)
+    if len(l):
+        return l[0]
+    return l
+
+def given_key(node, query, negchar=NEGCHAR):
+    """
+    (node, query_str) -> cpt_key
+    e.g.
+    >>> (e['m'], 'e, b, a, m, j') -> 'a'
+    >>> (e['m'], 'e, b, !a, m, j') -> '!a'
+    >>> cpt(e['m'])
+    {'a': 0.7, '!a': 0.01}
+    """
+    return first(filter(choose_key(query), keys(node)))
+
+#
+#   Bayes Net Functions
+#
+
+def bnet(*bnodes):
+    """
+    list_of_bnodes -> dict_of_bnodes 
+    """
+    return dict([(abbr(node), node) for node in bnodes])
+
+def bnet_nodes(bnet):
+    return bnet.values()
+
+def key_set(bnet):
+    return set(bnet.keys())
+
+def is_positive(node, query, negchar=NEGCHAR):
+    """
+    (node, query_str) -> Bool
+    e.g.
+        >>> ('e', 'j,m,a,!b,!e')
+        False
+    """
+    f = list(filter(lambda abbrv: abbr(node) in abbrv, query))
+    if len(f):
+        return negchar not in f[0]
+    return False
+
+def unique(tup, negchar=NEGCHAR):
+    """
+    predicate for whether or not a tuple contains the same abbr
+
+    e.g.
+        >>> bn.unique(('a', '!a')) 
+        False
+        >>> bn.unique(('a', '!b'))
+        True
+    """
+    return len(set(map(lambda x: x.split(negchar)[-1], tup))) == \
+           len(list(map(lambda x: x.split(negchar)[-1], tup)))
+
+def remove_alike(abbr_list, negchar=NEGCHAR):
+    """
+    removes tuples like ('a', '!a'),
+                        ('c', '!a', '!c')
+    e.g.
+        >>> bn.remove_alike([('a', '!a'), ('b', 'c')]) 
+        [('b', 'c')]
+    """
+    return list(filter(unique, abbr_list))
+
+def combinate(ndlist):
+    """
+    takes a list of thingies and returns all possible permutations (with !) of
+    those variables
+
+    e.g.
+        >>> bn.combinate(['a', 'b'])
+        [('!a', '!b'), ('!a', 'b'), ('!b', 'a'), ('a', 'b')]
+    """
+    return remove_alike(it.combinations(
+            ([node_abbr_not(node) for node in ndlist] + list(ndlist)),
+            len(ndlist)))
+
+def remove_neg(n, negchar=NEGCHAR):
+    """
+    rv_abbr -> positive_rv_abbr
+        >>> bn.remove_neg('!a')
+        'a'
+        >>> bn.remove_neg('a')
+        'a'
+    """
+    if negchar in n:
+        return n.split(negchar)[-1]
+    else:
+        return n
+
+def hidden_vars(bnet, rv, given):
+    """
+    (bnet, rv, given) -> hidden_vars
+    """
+    return key_set(bnet) - set(map(remove_neg, rv + given))
+
+def alpha(a, b):
+    """
+    alpha is a normalization constant
+    """
+    return 1 / (a + b)
+
+def normalize(a, b):
+    """
+    pr(A), pr(B) -> alpha(a,b)*pr(A)
+
+    e.g.
+        >>> a = 0.03
+        >>> b = 0.0004
+        >>> a_norm = bn.normalize(a, b)
+        >>> b_norm = bn.normalize(b, a)
+        >>> print(a_norm, b_norm) 
+        0.9868421052631577, 0.013157894736842105
+    """
+    return alpha(a, b) * a
+
+def generate_queries(bnet, parsed_query):
+    """
+    bnet, query -> [query]
+
+    # debugging lol
+    """
+    rv, given = parsed_query
+    combos = combinate(hidden_vars(bnet, rv, given))
+    return [set(x).union(rv).union(given) for x in combos]
+
+def calc_pr(bnet, query):
+    """
+    (bnet, query) -> float
+    """
+    return product([node_pr(node, given_key=given_key(node, query),
+                                  is_pos=is_positive(node, query))
+                    for node in bnet_nodes(bnet)])
+
+def pr(bnet, query_string):
+    return normalize(sum([calc_pr(bnet, query) for query in
+                          generate_queries(bnet, parse_query(query_string))]),
+                     sum([calc_pr(bnet, query) for query in 
+                          generate_queries(bnet, parse_query(query_string, do_negate=True))]))
+        
+
+#
+# Models
+#
+
+def make_eq_model():
+    """
+    Generates the basic burglary & earthquake model from 
+    Artificial Intelligence: A Modern Approach
+    """
+    ecpt = { (): 0.002 }
+    bcpt = { (): 0.001 }
+    acpt = { ('b', 'e'): 0.95,
+             ('b', '!e'): 0.94,
+             ('!b', 'e'): 0.29,
+             ('!b', '!e'): 0.001 }
+    mcpt = { ('a',): 0.70,
+             ('!a',): 0.01 }
+    jcpt = { ('a',): 0.90,
+             ('!a',): 0.05 }
+    e = node('Earthquake', 'e', ecpt, parents=[])
+    b = node('Burgler',    'b', bcpt, parents=[])
+    a = node('Alarm',      'a', acpt, parents=[e,b])
+    m = node('Mary',       'm', mcpt, parents=[a])
+    j = node('John',       'j', jcpt, parents=[a])
+    bn = bnet(e,b,a,m,j)
     return bn
 
+def make_writer_model():
+    """
+    resume    writing sample
+    |               |
+    +-> R       W <-+
+         \     /
+          \   /
+            A <- accepted
+            |
+            J <- accept job
+    """
+    rcpt = { (): 0.25 }
+    wcpt = { (): 0.3 }
+    acpt = { ('r', 'w'): 0.95,
+             ('r', '!w'): 0.8,
+             ('!r', 'w'): 0.25,
+             ('!r', '!w'): 0.03 }
+    jcpt = { ('a',): 0.4,
+             ('!a',): 0 }
+    r = node('GoodResume', 'r', rcpt, parents=[])
+    w = node('GoodWritingSample', 'w', wcpt, parents=[])
+    a = node('Accepted', 'a', acpt, parents=[r,w])
+    j = node('TakesJob', 'j', jcpt, parents=[a])
+    bn = bnet(r,w,a,j)
+    return bn
 
 def run():
-    make_earthquake_model()
+    make_eq_model()
 
 if __name__ == '__main__':
     run()
-
